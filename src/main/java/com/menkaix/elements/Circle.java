9c1183739e62cd52b042c73dc0955bb3f3ce4a98
menkaix <mendrika.irina@gmail.com>
2024-03-31 14:31:35 +0300

    circle done

---
+++ src/main/java/com/menkaix/elements/Circle.java
@@ -0,0 +1,84 @@
+package com.menkaix.elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.menkaix.geometry.basic.PointCouple;
+import com.menkaix.geometry.components.SimplePoint;
+import com.menkaix.project.Behaviour;
+import com.menkaix.project.Geometry;
+import com.menkaix.project.RotationDirection;
+import com.menkaix.writegcode.ArcGcodePath;
+import com.menkaix.writegcode.ClosedLineGcodePath;
+
+public class Circle implements Element {
+
+	private static final long serialVersionUID = -3192960479843484058L;
+
+	private ArrayList<Behaviour> behaviours = new ArrayList<Behaviour>();
+
+	private String name;
+
+	private SimplePoint center;
+	private double radius;
+
+	public Circle(SimplePoint center, double radius) {
+
+		setCenter(center);
+		setRadius(radius);
+
+		updateGeomtry();
+	}
+
+	private void updateGeomtry() {
+
+		for (Behaviour behaviour : behaviours) {
+			if (behaviour instanceof Geometry) {
+				behaviours.remove(behaviour);
+			}
+		}
+		
+		SimplePoint p1 = new SimplePoint(center.getX()-radius, center.getY());
+		SimplePoint p2 = new SimplePoint(center.getX()+radius, center.getY());
+		
+		PointCouple points = new PointCouple(p1, p2);
+		
+		behaviours.add(new ArcGcodePath(points, RotationDirection.CLOCKWISE, radius));
+		behaviours.add(new ArcGcodePath(points, RotationDirection.COUNTER_CLOCKWISE, radius));
+
+	}
+
+	@Override
+	public String getElementName() {
+
+		return name;
+	}
+
+	@Override
+	public void setElementName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public List<Behaviour> getBehaviours() {
+
+		return behaviours;
+	}
+
+	public SimplePoint getCenter() {
+		return center;
+	}
+
+	public void setCenter(SimplePoint center) {
+		this.center = center;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+}
--- src/main/java/com/menkaix/pcbgcode/main/Main.java
+++ src/main/java/com/menkaix/pcbgcode/main/Main.java
@@ -1,6 +1,7 @@
 package com.menkaix.pcbgcode.main;
 
 import com.menkaix.elements.ArcPath;
+import com.menkaix.elements.Circle;
 import com.menkaix.elements.PolyLineElement;
 import com.menkaix.elements.Rectangle;
 import com.menkaix.geometry.components.SimplePoint;
@@ -9,7 +10,6 @@ import com.menkaix.project.BitHead;
 import com.menkaix.project.GcodeProject;
 import com.menkaix.project.Layer;
 import com.menkaix.project.RotationDirection;
-import com.menkaix.writegcode.GcodeFileWriter;
 
 /**
  *
@@ -34,12 +34,15 @@ public class Main {
 		PolyLineElement line = new PolyLineElement();
 		line.setElementName("coude");
 		line.addPoint(25, 30);
-		line.addPoint(18, 20);
+		line.addPoint(22, 20);
 		line.addPoint(34, 14);
+		
+		Circle circle = new Circle(new SimplePoint(27, 10), 5);
 
 		work.getElements().add(new Rectangle("Power Switch", 10, 10, 10, 12));
 		work.getElements().add(line);
 		work.getElements().add(arc);
+		work.getElements().add(circle);
 
 		try {
 			prj.addLayer(work);
